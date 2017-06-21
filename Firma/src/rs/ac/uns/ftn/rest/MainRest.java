f91defa7a11bb70ecfd12116fd2f67125793ef28
skilja95 <milivojeskiljevic7@gmail.com>
2017-06-21 02:28:39 +0200

    Zahtjevi za izvod
    Napravljena html stranica za ucitavanje xml-zahtjevi za izvod
    Pogodjen rest kontroler, napravljen objekat od xml-a, i poslat na servis firme.
    Potrebno jos objekat Presek, koji se dobije od banke, sacuvati u XML file

--- Firma/src/rs/ac/uns/ftn/rest/MainRest.java
+++ Firma/src/rs/ac/uns/ftn/rest/MainRest.java
@@ -16,46 +16,87 @@ import javax.xml.ws.Service;
 
 import rs.ac.uns.ftn.firma.Firma;
 import rs.ac.uns.ftn.nalogzaprenos.NalogZaPrenos;
+import rs.ac.uns.ftn.zahtevzaizvod.ZahtevZaIzvod;
 import utils.Pom;
-
 
 @Path("/main")
 public class MainRest {
-	
-    @POST
-    @Path("/loadFile/")
-    @Consumes({MediaType.APPLICATION_XML})
-    public String loadFile(Pom pom) throws JAXBException, MalformedURLException {
-    	try {
-       System.out.println("\nPogodio load file rest controller");
-       System.out.println("\nJAXB unmarshalling.\n");
-		System.out.println("\nPrimio file sa nazivom " + pom.getSearchField().trim());
-		String nazivFajla = pom.getSearchField().trim();
-		// Definiše se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
-		JAXBContext context = JAXBContext.newInstance("rs.ac.uns.ftn.nalogzaprenos");
-		
-		// Unmarshaller je objekat zadužen za konverziju iz XML-a u objektni model
-		Unmarshaller unmarshaller = context.createUnmarshaller();
-		
-		// Unmarshalling generiše objektni model na osnovu XML fajla 
-		NalogZaPrenos nalog = (NalogZaPrenos) unmarshaller.unmarshal(new File("C:\\Users\\skilj\\Desktop\\Firma\\xmlovi\\"+nazivFajla+".xml"));
-        System.out.println("\nXML fajl je uspjesno ucitan");
-        /*
-         * Kreiranje konekcije ka servisu FIRME
-         * */
-        URL wsdl = new URL("http://localhost:8080/firma/services/Firma?wsdl");
-    	QName serviceName = new QName("http://www.ftn.uns.ac.rs/firma", "FirmaService");
-    	QName portName = new QName("http://www.ftn.uns.ac.rs/firma", "FirmaPort");
-    	
-    	Service service = Service.create(wsdl, serviceName);
-    	
-    	Firma firma = service.getPort(portName,Firma.class);
 
-    	firma.posaljiNalogZaPrenos(nalog);
-    	return "OK";
-    	} catch (Exception e) {
-    		System.out.println("Error in reading file.Check file name!");
-    		return "ERROR";
-    	}
-    }
+	@POST
+	@Path("/loadFile/")
+	@Consumes({ MediaType.APPLICATION_XML })
+	public String loadFile(Pom pom) throws JAXBException, MalformedURLException {
+		try {
+			System.out.println("\nPogodio load file rest controller");
+			System.out.println("\nJAXB unmarshalling.\n");
+			System.out.println("\nPrimio file sa nazivom " + pom.getSearchField().trim());
+			String nazivFajla = pom.getSearchField().trim();
+			// Definiše se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
+			JAXBContext context = JAXBContext.newInstance("rs.ac.uns.ftn.nalogzaprenos");
+
+			// Unmarshaller je objekat zadužen za konverziju iz XML-a u
+			// objektni model
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+
+			// Unmarshalling generiše objektni model na osnovu XML fajla
+			NalogZaPrenos nalog = (NalogZaPrenos) unmarshaller
+					.unmarshal(new File("C:\\Users\\skilj\\Documents\\GitHub\\XMLWS\\Firma\\xmlovi\\" + nazivFajla + ".xml"));
+			System.out.println("\nXML fajl je uspjesno ucitan");
+			/*
+			 * Kreiranje konekcije ka servisu FIRME
+			 */
+			URL wsdl = new URL("http://localhost:8080/firma/services/Firma?wsdl");
+			QName serviceName = new QName("http://www.ftn.uns.ac.rs/firma", "FirmaService");
+			QName portName = new QName("http://www.ftn.uns.ac.rs/firma", "FirmaPort");
+
+			Service service = Service.create(wsdl, serviceName);
+
+			Firma firma = service.getPort(portName, Firma.class);
+
+			firma.posaljiNalogZaPrenos(nalog);
+			return "OK";
+		} catch (Exception e) {
+			System.out.println("Error in reading file.Check file name!");
+			return "ERROR";
+		}
+	}
+
+	@POST
+	@Path("/loadFileZZI/")
+	@Consumes({ MediaType.APPLICATION_XML })
+	public String loadFileZZI(Pom pom) throws JAXBException, MalformedURLException {
+		try {
+			System.out.println("\nPogodio load file rest controller");
+			System.out.println("\nJAXB unmarshalling.\n");
+			System.out.println("\nPrimio file sa nazivom " + pom.getSearchField().trim());
+			String nazivFajla = pom.getSearchField().trim();
+			// Definiše se JAXB kontekst (putanja do paketa sa JAXB bean-ovima)
+			JAXBContext context = JAXBContext.newInstance("rs.ac.uns.ftn.zahtevzaizvod");
+
+			// Unmarshaller je objekat zadužen za konverziju iz XML-a u
+			// objektni model
+			Unmarshaller unmarshaller = context.createUnmarshaller();
+
+			// Unmarshalling generiše objektni model na osnovu XML fajla
+			ZahtevZaIzvod zahtev = (ZahtevZaIzvod) unmarshaller
+					.unmarshal(new File("C:\\Users\\skilj\\Documents\\GitHub\\XMLWS\\Firma\\xmlovi\\" + nazivFajla + ".xml"));
+			System.out.println("\nXML fajl je uspjesno ucitan");
+			/*
+			 * Kreiranje konekcije ka servisu FIRME
+			 */
+			URL wsdl = new URL("http://localhost:8080/firma/services/Firma?wsdl");
+			QName serviceName = new QName("http://www.ftn.uns.ac.rs/firma", "FirmaService");
+			QName portName = new QName("http://www.ftn.uns.ac.rs/firma", "FirmaPort");
+
+			Service service = Service.create(wsdl, serviceName);
+
+			Firma firma = service.getPort(portName, Firma.class);
+
+			firma.posaljiZahtevZaIzvod(zahtev);
+			return "OK";
+		} catch (Exception e) {
+			System.out.println("\nError in reading file. Check file name!");
+			return "ERROR";
+		}
+	}
 }
